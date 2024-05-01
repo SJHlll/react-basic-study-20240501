3dfb82ba1183909de0c194f11b38288b526c079b
SJHlll <ssaadd1212@gmail.com>
2024-05-01 17:15:42 +0900

    2. 동적 데이터 렌더링 및 props 개념 확인

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-import NoName from './NoName';
+import React from 'react'
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
 
-  const $h2 = <h2>ㅎㅇ</h2>;
+  //지출 항목 객체 배열
+
+  const expenses = [
+    {
+      title: '바나나',
+      price: 2000,
+      date: new Date(2023, 3, 23),
+    },
+    {
+      title: 'BBQ치킨',
+      price: 20000,
+      date: new Date(2023, 5, 21),
+    },
+    {
+      title: '도미노피자',
+      price: 35000,
+      date: new Date(2023, 7, 4),
+    },
+  ];
 
   return (
     <>
-    <NoName />
-      <div className='App'>
-        <h1>ㅎㅇ</h1>
-        {$h2}
-      </div>
-      <div className='noname'>
-        <input type='text' placeholder='ㅂㅂ'/>
-        <p>
-          반가워<br />
-          나는나야
-        </p>
-      </div>
+      <ExpenseItem date={expenses[0].date} title={expenses[0].title} price={expenses[0].price} />
+      <ExpenseItem date={expenses[1].date} title={expenses[1].title} price={expenses[1].price} />
+      <ExpenseItem date={expenses[2].date} title={expenses[2].title} price={expenses[2].price} />
     </>
   );
 }
