0ac4180cfabb3066586b1bf83fb289a8d2fc0e33
SJHlll <ssaadd1212@gmail.com>
2024-05-02 16:56:39 +0900

    8. 동적 리스트 렌더링, 연도별 필터링

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ function App() {
   //지출 항목 객체 배열
   const expenses = [
     {
+      id: 1,
       title: '바나나',
       price: 3000,
       date: new Date(2023, 3 - 1, 23),
     },
     {
+      id: 2,
       title: 'BBQ치킨',
       price: 20000,
       date: new Date(2023, 5 - 1, 21),
     },
     {
+      id: 3,
       title: '도미노피자',
       price: 35000,
       date: new Date(2023, 7 - 1, 4),
+    },
+    {
+      id: 4,
+      title: '엽기떡볶이',
+      price: 18000,
+      date: new Date(2024, 6 - 1, 1),
     },
   ];
 
@@ -27,6 +36,7 @@ function App() {
   const addExpenseHandler = (NewExpense) => {
     console.log('App 컴포넌트 응답');
     console.log('newExpense : ', NewExpense);
+    expenses.push(NewExpense);
   };
 
   return (
