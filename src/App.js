882c729b26a1a80134a64b1edd5e9268b7f1a031
SJHlll <ssaadd1212@gmail.com>
2024-05-02 12:48:14 +0900

    4. props.children 학습

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import './App.css';
-import React from 'react'
+import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses';
+import Hello from './Hello';
 
 function App() {
-
   //지출 항목 객체 배열
-
   const expenses = [
     {
       title: '바나나',
-      price: 2000,
-      date: new Date(2023, 3-1, 23),
+      price: 3000,
+      date: new Date(2023, 3 - 1, 23),
     },
     {
       title: 'BBQ치킨',
       price: 20000,
-      date: new Date(2023, 5-1, 21),
+      date: new Date(2023, 5 - 1, 21),
     },
     {
       title: '도미노피자',
       price: 35000,
-      date: new Date(2023, 7-1, 4),
+      date: new Date(2023, 7 - 1, 4),
     },
   ];
 
   return (
     <>
       <Expenses items={expenses} />
+      <Hello>
+        <ul>
+          <li>사과</li>
+          <li>포도</li>
+          <li>복숭아</li>
+        </ul>
+      </Hello>
     </>
   );
 }
